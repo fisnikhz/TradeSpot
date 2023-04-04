710e10f2b56cdb1f433086bc11ff1f4e32146989
Fisnik Hazrolli <71229274+fisnikhz@users.noreply.github.com>
2023-04-04 15:41:58 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,205 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <title>Projekti Ueb2</title>
+  <link rel="stylesheet" href="css/hp.css">
+  <link rel="stylesheet" href="css/login.css">
+  <style>
+    /* The Modal (background) */
+    .modal {
+      display: none;
+      position: fixed;
+      z-index: 1;
+      left: 0;
+      top: 0;
+      margin-left: 80px;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    /* Modal Content/Box */
+    .modal-content {
+      background-color: #fefefe;
+      padding: 20px;
+      border: 1px solid #888;
+      width: 250px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    /* The Close Button */
+    .close {
+      color: #333;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      font-size: 28px;
+      font-weight: bold;
+      text-align: center;
+      line-height: 1;
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      background-color: black;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .close:hover,
+    .close:focus {
+      color: #fff;
+      text-decoration: none;
+    }
+  </style>
 </head>
+
 <body>
-    
+  <div id="myModal" class="modal">
+    <div class="modal-content">
+      <span class="close">&times;</span>
+      <h3 id="modal_message" style="color: #f00;"></h3>
+    </div>
+  </div>
+  <div class="navbar">
+    <h1 class="logo">TradeSpot</h1>
+    <a href="homepage.php">Home</a>
+    <a href="profile.php">Profile</a>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Contact</a>
+  </div>
+  <div class="content">
+    <?php
+    // establish database connection
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "ueb2";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // check connection
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // handle login form submission
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
+      $username = mysqli_real_escape_string($conn, $_POST['username_or_email']);
+      $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+      $sql = "SELECT * FROM users WHERE username='$username' OR email='$username'";
+      $result = mysqli_query($conn, $sql);
+
+      if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row['password'])) {
+          // password is correct, start session and redirect to homepage or dashboard depending on user role
+          session_start();
+          $_SESSION['user_id'] = $row['id'];
+          if ($row['email'] == 'admin@admin.com' && $password == 'admin' || ($row['username'] == 'admin')) {
+            header("Location: dashboard.php");
+          } else {
+            header("Location: homepage.php");
+          }
+          exit;
+        } else {
+          echo ' <script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // Show the modal with a message
+    function showError(message) {
+      var modalMessage = document.getElementById("modal_message");
+      modalMessage.innerHTML = message;
+      modal.style.display = "block";
+    }
+    showError("Incorrect password");
+  </script>';
+        }
+      } else {
+        // user does not exist, show error message
+        echo ' <script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // Show the modal with a message
+    function showError(message) {
+      var modalMessage = document.getElementById("modal_message");
+      modalMessage.innerHTML = message;
+      modal.style.display = "block";
+    }
+    showError("User not found");
+  </script>';
+      }
+    }
+
+    mysqli_close($conn);
+    ?> <div class="form-box">
+      <div class="form">
+        <form class="login-form" action="login.php" method="post">
+          <h1 id="login-txt">Login</h1>
+
+          <label id="u_or_e" for="username_or_email">Username</label>
+          <input type="text" id="usernamebox" name="username_or_email">
+          <label id="password" for="password">Password</label>
+          <input type="password" id="passwordbox" name="password">
+          <p id="signup-link">Dont have a account, <a href="signup.php">sign up.</a></p>
+          <input type="submit" name="login" value="Login">
+        </form>
+      </div>
+    </div>
+    </h1>
+  </div>
+
+  <script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // window.onload = function() {
+    //         modal.style.display = "none";
+
+    // };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // Show the modal with a message
+    function showError(message) {
+      var modalMessage = document.getElementById("modal_message");
+      modalMessage.innerHTML = message;
+      modal.style.display = "block";
+    }
+    showError("Incorrect password");
+  </script>
 </body>
+
 </html>
